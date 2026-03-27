0edda1633dbecde328674481a562deff7540df69
quang-doan-tpv-clv <dnq.httt@gmail.com>
2026-03-27 20:34:31 +0700

    Update helpers

--- helpers/join_models.py
+++ helpers/join_models.py
@@ -10,7 +10,7 @@ SENTENCE_TRANSFORMER_EMBEDDING_FILE_ID = '1lwjvkdgopOg5ioQ9j3NGWIKF1IbbBROP'
 SIM_MATRIX_SENTENCE_TRANSFORMER_FILE_ID = '1iK-HByrd5YPt8-On4XPKHDpuMQUMUTSd'
 
 SENTENCE_TRANSFORMER_MODEL_LOCAL_PATH = 'models/recommendation/sentence_transformer_model.pkl'
-SENTENCE_TRANSFORMER_EMBEDDING_LOCAL_PATH = 'models/recommendation/sentence_transformer_embedding.pkl'
+SENTENCE_TRANSFORMER_EMBEDDING_LOCAL_PATH = 'models/recommendation/sentence_transformer_embeddings.pkl'
 SIM_MATRIX_SENTENCE_TRANSFORMER_LOCAL_PATH = 'models/recommendation/sim_matrix_sentence_transformer.pkl'
 
 def join_models():
