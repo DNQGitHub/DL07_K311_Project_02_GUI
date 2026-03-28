8f3c9b7a3bb18be454bbe3272742a4f48561be30
quang-doan-tpv-clv <dnq.httt@gmail.com>
2026-03-28 23:30:06 +0700

    update weights for recommendation

--- features/recommendation/helpers/data_featuring.py
+++ features/recommendation/helpers/data_featuring.py
@@ -1,5 +1,31 @@
 import re
 
+def get_location_tokens():
+    locations = [
+        "tân sơn nhất",
+        "gia định",
+        "bình tân",
+        "thủ đức",
+        "bình thạnh",
+        "gò vấp",
+        "phú nhuận",
+        "tân bình",
+        "tân phú",
+        "bình chánh",
+        "cần giờ",
+        "củ chi",
+        "hóc môn",
+        "nhà bè"
+    ]
+    
+    for quan_so in range(1, 12):
+        quan = f"quận {quan_so}"
+        locations.append(quan)
+    
+    locations = [loc.replace(" ", "_") for loc in locations]
+    
+    return locations
+
 def extract_price(query):
     match = re.search(r"(\d+(\.\d+)?)\s*tỷ", query)
     if match:
@@ -7,7 +33,13 @@ def extract_price(query):
     return None
 
 def extract_district(query):
-    match = re.search(r"Quận\s*([\w\s]+)", query)
-    if match:
-        return match.group(1).strip().lower().replace(" ", "_")
+    locations = get_location_tokens()
+
+    if query is None:
+        return "unknown"
+   
+    for loc in locations:
+        if loc in  query:
+            return loc
+
     return "unknown"
--- features/recommendation/helpers/recommend_posts_by_query.py
+++ features/recommendation/helpers/recommend_posts_by_query.py
@@ -4,9 +4,7 @@ from features.recommendation.helpers.load_data import load_recommendation_embedd
 from features.recommendation.helpers.data_preprocessing import preprocess_query
 from features.recommendation.helpers.data_featuring import extract_district, extract_price
 
-def price_sim(price_query, price_series, alpha=1.0):
-    print(f"Price Query: {price_query}")
-    
+def price_sim(price_query, price_series, alpha=1.0):    
     if price_query == 0 or price_query is None:
         return np.ones(len(price_series))
     
@@ -21,14 +19,19 @@ def normalize_score(x):
         return x * 0
     return (x - x.min()) / (x.max() - x.min())
 
-def recommend_posts_by_query(
-    query,
-    df,
-    top_k=5,
-    w_bert=0.6,
-    w_price=0.2,
-    w_location=0.2
-):
+def pick_weights(target_loc, target_price): # output: w_bert, w_price, w_location
+    if target_loc == "unknown" and target_price is None:
+        return 0.6, 0.2, 0.2
+    elif target_loc == "unknown" and target_price is not None:
+        return 0.4, 0.5, 0.1
+    elif target_loc != "unknown" and target_price is None:
+        return 0.3, 0.1, 0.6
+    elif target_loc != "unknown" and target_price is not None:
+        return 0.2, 0.3, 0.5
+    else:
+        return 0.6, 0.2, 0.2
+
+def recommend_posts_by_query(query, df, top_k=5):
     model = load_recommendation_model()
     embeddings = load_recommendation_embeddings()
     
@@ -49,6 +52,8 @@ def recommend_posts_by_query(
     bert_norm = normalize_score(bert_scores)
     price_norm = normalize_score(price_scores)
     loc_norm = normalize_score(location_scores)
+    
+    w_bert, w_price, w_location = pick_weights(target_loc, target_price)
     
     final_score = (
         w_bert * bert_norm +
